fb5fc4b34bd8048f4038c828843b0fe3b1f1d840
Pavan699 <pavannakate03@gmail.com>
2021-09-05 04:08:05 -0700

    Display Minimum Notes for Amount

---
+++ Vending_Machine/ATM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vending_Machine
+{
+    class ATM
+    {
+        int[] notes = new int[] {1000,500,100,50,10,5,2,1};
+        int[] counter = new int[8];
+        public void Check(int rupees)
+        {
+            for (int i = 0 ; i < notes.Length ; i++)
+            {
+                if (rupees >= notes[i])
+                {
+                    counter[i] = rupees / notes[i] ;
+                    rupees = rupees - counter[i] * notes[i];
+                }
+            }
+            for(int j = 0 ; j < notes.Length ; j++)
+            {
+                if(counter[j] != 0)
+                {
+                    Console.WriteLine(notes[j]+" ==> "+counter[j]);
+                }
+            }
+        }
+    }
+}
---
+++ Vending_Machine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Vending Machine Program");
+            int Rs;
+            Console.Write("Enter The Ammount To Withdrow : ");
+            Rs = Convert.ToInt32(Console.ReadLine());
+            ATM a = new ATM();
+            a.Check(Rs);
+        }
+    }
+}
