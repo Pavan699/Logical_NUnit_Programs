502a036bf3930ebac161f10d4dba723c26e07e38
Pavan699 <pavannakate03@gmail.com>
2021-09-05 04:15:19 -0700

    Monthly Pay For the Loan

---
+++ Monthly_Payment/Calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monthly_Payment
+{
+    class Calculate
+    {
+        public static void monthlyPayment(double amount,double year,double intrest)
+        {
+            double total_Amount,r,n;
+            n = 12 * year;
+            r = intrest / (12*100);
+            total_Amount = (amount * r) / (1 - Math.Abs(Math.Pow((1+r),(-n))));
+            Console.WriteLine("The Total Monthly Payment is : " + total_Amount);
+            
+        }
+    }
+}
---
+++ Monthly_Payment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Monthly_Payment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program To Calculate Monthly Payment");
+            double Y, P, R;
+            Console.Write("Enter The Ammount Of Loan : ");
+            P = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter The Year For Loan : ");
+            Y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter The Intrest Percentage For the loan : ");
+            R = Convert.ToInt32(Console.ReadLine());
+            Calculate.monthlyPayment(P, Y, R);
+        }
+    }
+}
