6e21431f76173c021b205f2a0f319b3f11d76b4d
Pavan699 <pavannakate03@gmail.com>
2021-09-05 04:13:09 -0700

    Conversion Of Celsius and Fehranhite

---
+++ Temperatur_Conversion/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Temperatur_Conversion
+{
+    class Calculate
+    {
+        public static void tempCal(double t)
+        {
+            int num;
+            Console.Write("Entered the Value is Celsius==1 OR Fahranheit==2 : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            double fahrenheit, celsius;
+            if (num == 1)
+            {
+                celsius = t;
+                fahrenheit = Math.Abs((celsius * (1.8)) + 32);
+                Console.WriteLine("If Temperatur is In Celsiun {0} then Converted Fehrenhnit is : {1}", t, fahrenheit);
+            }
+            if (num == 2)
+            {
+                fahrenheit = t;
+                celsius = (Math.Abs(fahrenheit - 32) * (0.55));
+                Console.WriteLine("If Temperatur is In Fehrenhnit {0} then Converted Celsiun is : {1}", t, celsius);
+            }
+            if (num != 1 && num != 2)
+            {
+                Console.WriteLine("Enter Wrong Input :)");
+            }
+        }
+    }
+}
---
+++ Temperatur_Conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Temperatur_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperatur Conversion Program");
+            double temp;
+            Console.Write("Enter The Temperatur : ");
+            temp = Convert.ToInt32(Console.ReadLine());
+            Calculate.tempCal(temp);
+        }
+    }
+}
