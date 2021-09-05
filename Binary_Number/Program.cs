8bef68a400f9d45da3b95f507658e07fbe6529aa
Pavan699 <pavannakate03@gmail.com>
2021-09-05 04:16:50 -0700

    Decimal to Binary Conversion

---
+++ Binary_Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Binary_Number
+{
+    class Program
+    {
+        static void toBinary(int num)
+        {
+            int i;
+            int[] a = new int[9];
+            for (i = 0; num > 0; i++)
+            {
+                a[i] = num % 2;
+                num = num / 2;
+            }
+            Console.Write("Binary Numbers is : ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Number Conversion Program");
+            int Number;
+            Console.Write("Enter The Number to convert in Binary : ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            toBinary(Number);
+        }
+    }
+}
