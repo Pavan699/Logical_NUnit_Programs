16dc9e4be6dd2455d894670c6e7085b83045cbff
Pavan699 <pavannakate03@gmail.com>
2021-09-05 04:10:16 -0700

    Display Day for Entered Date

---
+++ Day_of_Week/DayofWeek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_of_Week
+{
+    class DayofWeek
+    {
+        public static void dayOfWeek(int m,int d,int y)
+        {
+            int y0, m0, d0,x;
+            string[] days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Firday","Saturday"};
+            string[] month = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept","Oct","Nov","Dec"};
+            y0 = y - ((14 - m) / 12);
+            x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 40);
+            m0 = m + 12 * ((14 - m) / 12) - 2;   
+            d0 = (d + x + 31 + (m0 / 12)) % 7;
+            Console.WriteLine("The Day,Month and Year of Entered Date is : {0}_{1}_{2}",days[d0-1],month[m-1],y0);
+        }
+    }
+}
---
+++ Day_of_Week/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day of week Program");
+            int m, d, y;
+            Console.Write("Enter the Month :");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the day of the Month :");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Year :");
+            y = Convert.ToInt32(Console.ReadLine());
+            DayofWeek.dayOfWeek(m,d,y); 
+        }
+    }
+}
