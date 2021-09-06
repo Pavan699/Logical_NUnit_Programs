b8366fbe208531ce82a0c0fd66eb58b86ef32814
Pavan699 <pavannakate03@gmail.com>
2021-09-06 07:31:10 -0700

    Added Square Root

---
+++ Sqrt_Method/CalRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sqrt_Method
+{
+    class CalRoot
+    {
+        public static double SqrtFun(double n, double ep)
+        {
+            double t = n;
+            double root;
+            int count = 0;
+            while (true)
+            {
+                count++;
+                root = 0.5 * (t + (n / t));
+
+                if (Math.Abs(root - t) < ep)
+                    break;
+                t = root;
+            }
+            return t;
+        }
+    }
+}
---
+++ Sqrt_Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sqrt_Method
+{
+    class Program
+    {
+        public static double SqrtFun(double n, double ep)
+        {
+            double t = n;
+            double root;
+            int count = 0;
+            while (true)
+            {
+                count++;
+                root = 0.5 * (t + (n / t));
+
+                if (Math.Abs(root - t) < ep)
+                    break;
+                t = root;
+            }
+            return t;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program To Compute Square Root.");
+            Console.Write("Enter the Number To check the SquareRoot :");
+            double c = double.Parse(Console.ReadLine());
+            double epsilon = 0.000000000000001;
+            SqrtFun(c,epsilon);
+        }
+    }
+}
